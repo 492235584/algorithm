469fc83d1221883a80550f33f507d180677021d9
492235584 <492235584@qq.com>
2017-10-17 22:54:31 +0800

    init

---
+++ algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by 谢加浩 on 2017/10/17.
+//  Copyright © 2017年 谢加浩. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
